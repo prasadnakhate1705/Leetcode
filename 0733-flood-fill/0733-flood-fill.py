2e2293bda7bd56bcbe28a7980ba0404e2223860e
prasadnakhate1705 <104776689+prasadnakhate1705@users.noreply.github.com>
2025-07-02 15:38:26 -0400

    Time: 0 ms (100%), Space: 18.1 MB (28.01%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -3,21 +3,24 @@ class Solution:
 
         rows = len(image)
         cols = len(image[0])
-        visited = []
+        visited = {(sr, sc)}
         og_color = image[sr][sc]
         directions = [[1,0], [0,1], [-1,0], [0,-1]]
 
-        def bfs(i, j, color):
+        if og_color == color:
+            return image
+
+        def dfs(i, j, color):
 
             image[i][j]=color
-            visited.append([i,j])
+            visited.add((i,j))
 
             for x,y in directions:
-                if 0<=i+x<rows and 0<=j+y<cols and image[i+x][j+y]==og_color and [i+x, j+y] not in visited:
-                    bfs(i+x, j+y,color)
+                if 0<=i+x<rows and 0<=j+y<cols and image[i+x][j+y]==og_color and (i+x, j+y) not in visited:
+                    dfs(i+x, j+y,color)
 
 
-        bfs(sr,sc,color)
+        dfs(sr,sc,color)
 
         return image            
 
