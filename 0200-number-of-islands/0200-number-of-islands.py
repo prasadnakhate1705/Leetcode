25aa13dc0fe2ec7ef1180edb465019d5fd98fa7c
prasadnakhate1705 <104776689+prasadnakhate1705@users.noreply.github.com>
2025-07-02 16:12:45 -0400

    Time: 252 ms (50.14%), Space: 20.4 MB (42.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,60 +1,38 @@
 from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # '1' - Land
+        # '0' - Water
+        rows, cols = len(grid), len(grid[0])
+        direction = [(0,1), (1,0), (-1,0), (0,-1)]
+        islands =0
 
-        rows = len(grid)
-        cols = len(grid[0])
-        direction = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        islands=0
+        def bfs(i,j):
+            q = deque([(i,j)])
+            grid[i][j]='0'
+            visited = {(i,j)}
 
-        # def dfs(i,j):
-
-        #     if  i<0 or i>rows-1 or j<0 or j> cols-1 or grid[i][j]=='0':
-        #         return 
-
-        #     grid[i][j] = '0'    
-
-        #     dfs(i + 1, j)  # Down
-        #     dfs(i - 1, j)  # Up
-        #     dfs(i, j + 1)  # Right
-        #     dfs(i, j - 1)  # Left
-
-
-        # for r in range(rows):
-        #     for c in range(cols):
-        #         if grid[r][c] == '1':  # Found an island
-        #             islands += 1
-        #             dfs(r, c)  # Sink the entire island
-
-        # return islands        
-
-        def bfs(r,c):
-            queue = deque([(r,c)])
-            grid[r][c] = '0'
-
-            while queue:
-                m,n = queue.popleft()
-
-                for nr, nc in direction:
-                    i,j = m+nr, n+nc
-                    if 0 <= i < rows and 0 <= j < cols and grid[i][j] == '1':
-                        queue.append((i,j))
-                        grid[i][j]='0'
+            while q:
+                i,j = q.popleft()
+                for di, dj  in direction:
+                    x,y = i+di, j+dj
+                    if 0<=x<rows and 0<=y<cols  and grid[x][y]=='1' and (x,y) not in visited:
+                        grid[x][y]='0'
+                        q.append((x,y))
+                        visited.add((x,y))
 
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1':  # Found an island
-                    islands += 1
-                    bfs(r, c)
-
-        return islands        
-            
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]=='1':
+                    islands+=1
+                    bfs(i,j)
+        
         
 
-            
+        return islands                
 
 
 
-        
         
