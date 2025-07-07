b3470091db758fa94e7961ff99b7e88ac621238c
prasadnakhate1705 <104776689+prasadnakhate1705@users.noreply.github.com>
2025-07-07 13:30:14 -0400

    Time: 3 ms (81.93%), Space: 17.6 MB (93.08%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -6,6 +6,7 @@ class Solution:
         # 0 - empty cell
 
         self.minutes = 0
+        self.fresh=0
         rows , cols = len(grid), len(grid[0])
         direction = [(1,0),(0,1), (0,-1), (-1,0)]
 
@@ -19,6 +20,7 @@ class Solution:
 
                 for di, dj in direction:
                     if 0<=di+i<rows and 0<=dj+j<cols and grid[di+i][dj+j]==1:
+                        self.fresh-=1
                         grid[di+i][dj+j]=2
                         q.append((di+i, dj+j,mini+1))
 
@@ -26,15 +28,16 @@ class Solution:
 
         for i in range(rows):
             for j in range(cols):
+                if grid[i][j]==1:
+                    self.fresh+=1
+                
                 if grid[i][j]==2:
                     q.append((i,j,0))
 
         bfs()  
 
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j]==1:
-                    return -1             
-
+        if self.fresh>0:
+            return -1
+                    
 
         return self.minutes  
