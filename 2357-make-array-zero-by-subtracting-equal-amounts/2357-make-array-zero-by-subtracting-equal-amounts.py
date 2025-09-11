7656257b47ce607a6102839a180a1c95d9888746
prasadnakhate1705 <104776689+prasadnakhate1705@users.noreply.github.com>
2025-09-11 15:19:02 -0400

    Time: 0 ms (100%), Space: 17.8 MB (52.02%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -2,34 +2,41 @@ import heapq
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
 
-        nums.sort()
-        count =0
-        x = 0
-        index =0
+        # nums.sort()  # nlogn
+        # count =0
+        # x = 0
+        # index =0
 
-        if sum(nums)==0:
-            return count
+        # if sum(nums)==0:
+        #     return count
+
+        # for i in range(len(nums)):
+        #     if nums[i]!=0:
+        #         index=i
+        #         break
+        
+        # while index!= len(nums):
+        #     x = nums[index]
+        #     count+=1
+        #     for i in range(len(nums)):
+        #         if nums[i]!=0:
+        #             nums[i]-=x
+                
+        #         if nums[i]==0:
+        #             index= i+1
+        #     print(nums)
+
+        # return count
+
+        unique_set = set()
 
         for i in range(len(nums)):
             if nums[i]!=0:
-                index=i
-                break
+                unique_set.add(nums[i])
         
-        while index!= len(nums):
-            x = nums[index]
-            count+=1
-            for i in range(len(nums)):
-                if nums[i]!=0:
-                    nums[i]-=x
-                
-                if nums[i]==0:
-                    index= i+1
-            
-            print(nums)
+        return len(unique_set)
 
-            
-        
-        return count
+
         
         
 
