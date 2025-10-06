cbdf65f20947c2b8c6268ac1920d7a5034f6258d
Prasad Nakhate <104776689+prasadnakhate1705@users.noreply.github.com>
2025-10-06 12:48:34 -0400

    Time: 0 ms (100%), Space: 17.8 MB (32.1%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,29 @@
+import heapq
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        
+        max_heap = []
+        for n in stones:
+            heapq.heappush(max_heap, -n)
+        
+        while len(max_heap)>1:
+            first = -heapq.heappop(max_heap)
+            second = -heapq.heappop(max_heap)
+
+            if first==second:
+                continue
+            else:
+                diff = abs(first-second)
+                heapq.heappush(max_heap, -diff)
+        
+        return -max_heap[0] if max_heap else 0
+
+            
+
+                
+
+
+
+
+        
