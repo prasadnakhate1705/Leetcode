77351d83aeaa7017c6426ec4d3b5a3a98a008ad9
Prasad Nakhate <104776689+prasadnakhate1705@users.noreply.github.com>
2025-11-05 16:21:21 -0500

    Time: 23 ms (30.27%), Space: 20.1 MB (53.19%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,31 +1,53 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        directions =[(1,0), (0,1)]
+        # directions =[(1,0), (0,1)] # Right and down
 
-        rows = len(grid)
-        cols = len(grid[0])
-        matrix = [[-1 for _ in range(cols)] for _ in range(rows)]
+        # rows = len(grid)
+        # cols = len(grid[0])
+
+        # # For memoziation
+        # matrix = [[-1 for _ in range(cols)] for _ in range(rows)]
         
-        def dp(r,c):
-            if r >= rows or c >= cols:
-                return float('inf')
+        # def dp(r,c):
+        #     if r >= rows or c >= cols:
+        #         return float('inf')
 
-            if r == rows - 1 and c == cols - 1:
-                return grid[r][c] 
+        #     if r == rows - 1 and c == cols - 1:
+        #         return grid[r][c] 
             
-            if matrix[r][c] != -1:
-                return matrix[r][c]
+        #     # Already stored in the Dp memo array
+        #     if matrix[r][c] != -1:
+        #         return matrix[r][c]
             
-            right = dp(r, c + 1)
-            down = dp(r + 1, c)
+        #     # two options right and down
+        #     right = dp(r, c + 1)
+        #     down = dp(r + 1, c)
 
-            matrix[r][c] = grid[r][c] + min(right, down)
+        #     #Update the table
+        #     matrix[r][c] = grid[r][c] + min(right, down)
 
-            return matrix[r][c]
+        #     return matrix[r][c]
+
+        #return dp(0,0)
         
+        rows, cols = len(grid), len(grid[0])
+        dp = [[float('inf')] * cols for _ in range(rows)]
+
+        dp[0][0] = grid[0][0]
         
+        for r in range(rows):
+            for c in range(cols):
+                if r > 0:
+                    dp[r][c] = min(dp[r][c], dp[r-1][c] + grid[r][c])
+                if c > 0:
+                    dp[r][c] = min(dp[r][c], dp[r][c-1] + grid[r][c])
+
+
+        return dp[-1][-1]
+
+
+
         
-        return dp(0,0)
             
 
 
