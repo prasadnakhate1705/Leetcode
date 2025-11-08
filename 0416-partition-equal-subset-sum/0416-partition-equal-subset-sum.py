1ee385d99131a85f2fc3e8b22bfded1ea1ca92d8
Prasad Nakhate <104776689+prasadnakhate1705@users.noreply.github.com>
2025-11-08 13:49:39 -0500

    Time: 2126 ms (15.88%), Space: 56.3 MB (22.94%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,24 +1,53 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
 
-        nums_bool = [False for _ in range(len(nums))]
+        n = len(nums)
+        target = sum(nums)//2
+        dp = [[-1]*(target+1) for _ in range(n)]
 
-        total = sum(nums)
-
-        if total % 2 ==1:
+        if sum(nums)%2==1:
             return False
+
+
+        def func(i, target):
+            if target==0:
+                return True
+            
+            if i==0:
+                return (nums[0]==target)
+            
+            if dp[i][target]!=-1:
+                return dp[i][target]
+            
+            not_take = func(i-1, target)
+            take = False
+
+            if nums[i]<=target:
+                take = func(i-1, target-nums[i])
+            
+            dp[i][target]= take or not_take
+
+            return dp[i][target]
         
-        target = total//2
+        return func(n-1,target)
 
-        dp = [False] * (target + 1)
 
-        dp[0] = True
-
-        for num in nums:
-            for t in range(target, num-1, -1):
-                dp[t] = dp[t] or dp[t-num]
+        # total = sum(nums)
+        # if total % 2 ==1:
+        #     return False
         
-        return dp[target]
+        # target = total//2
+
+        # # DP array
+        # dp = [False] * (target + 1)
+
+        # dp[0] = True
+
+        # for num in nums:
+        #     for t in range(target, num-1, -1):
+        #         dp[t] = dp[t] or dp[t-num]
+        
+        # return dp[target]
 
 
 
