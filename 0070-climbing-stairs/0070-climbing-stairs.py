df4c37544e5271c5b140cc56e057b327f1c3f992
Prasad Nakhate <104776689+prasadnakhate1705@users.noreply.github.com>
2026-01-04 15:49:10 -0500

    Time: 0 ms (100%), Space: 17.3 MB (80.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,7 +1,10 @@
 class Solution:
 
     def climbStairs(self, n: int) -> int:
+        
+        # This is similar to Fibbonacci series
 
+        # Memoization
         dp_table = [-1] * (n+1)
         
         def dpclimbStairs(k):
@@ -14,7 +17,16 @@ class Solution:
             dp_table[k] = dpclimbStairs(k-1) + dpclimbStairs(k-2)
 
             return dp_table[k]
+        
+        #return dpclimbStairs(n)
 
+        dp_table[0]=1
+        dp_table[1]=1
 
-        return dpclimbStairs(n)
+        for i in range(2,n+1):
+            dp_table[i] = dp_table[i-1]+ dp_table[i-2]
+
+        
+        return dp_table[n]
+
             
