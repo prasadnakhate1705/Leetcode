38f6e91a5b997fa66bb89fba9ef6c1a443ddec24
Prasad Nakhate <104776689+prasadnakhate1705@users.noreply.github.com>
2026-03-23 14:51:29 -0400

    Time: 64 ms (12.44%), Space: 22.4 MB (86.47%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+
+        n = len(word1)
+        m = len(word2)
+
+        dp_table = [[-1]*(m+1) for _ in range(n+1)]
+
+        def dp(i,j):
+            if i == 0:
+                return j  # all inserts
+            if j == 0:
+                return i  #all inserts
+            
+            if dp_table[i][j]!= -1:
+                return dp_table[i][j]
+            
+            if word1[i-1]==word2[j-1]:
+                return dp(i-1,j-1)
+            else:
+                dp_table[i][j]= 1+ min(dp(i-1, j), dp(i,j-1), dp(i-1,j-1))
+                return dp_table[i][j]
+        
+
+        return dp(n,m)
+        
+
+         
+            
+             
+                
+            
+            
+            
+
+            
+        
