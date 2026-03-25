7a023c24266bab4ec379e2a5d4f13e297529f621
Prasad Nakhate <104776689+prasadnakhate1705@users.noreply.github.com>
2026-03-25 13:21:30 -0400

    Time: 1 ms (87.31%), Space: 19.3 MB (45.44%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        hash_map = {'I': 1, 'V':5, 'X':10, 'L':50, 'C': 100, 'D':500, 'M':1000}
+        edge= {'IV':4, 'IX':9, 'XC':90, 'XL':40, 'CD':400, 'CM':900}
+
+        total = 0
+        i = 0
+        while i< len(s):
+            if s[i:i+2] in edge:
+                total+=edge[s[i:i+2]]
+                i+=2
+            else:
+                total+= hash_map[s[i]]
+                i+=1
+        
+        return total
+
+
+
+        
