14777e0844564a55fda8a4d7250d3044f2d62ce8
Prasad Nakhate <104776689+prasadnakhate1705@users.noreply.github.com>
2026-04-15 17:59:40 -0400

    Time: 7 ms (6.08%), Space: 19.4 MB (55.85%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -2,32 +2,29 @@ class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
 
         count = {}
-
         # hash_map
         for i in range(len(words)):
             if words[i] not in count:
                 count[words[i]]=0
             count[words[i]]+=1
         
-        
         res = []
+        for i, (key, value) in enumerate(count.items()):
+            res.append((key, value))
+        
+        for i in range(len(res)):
+            for j in range(0, len(res) - i - 1):
+                if res[j][1] < res[j+1][1] or \
+                   (res[j][1] == res[j+1][1] and res[j][0] > res[j+1][0]):
+                    res[j], res[j+1] = res[j+1], res[j]
+                
 
-        for i, (key,val) in enumerate(count.items()):
-            if len(res)<k:
-                heapq.heappush(res,(val,key))
-                continue
-            
-            if res[0][0] < val:
-                heapq.heappop(res)
-                heapq.heappush(res,(val,key))
-        
-        
-        ans =[]
+        ans = []
 
         for i in range(k):
-            ans.append(res[i][1])
+            ans.append(res[i][0])
 
-        return ans.sort()
+        return ans
             
 
         
